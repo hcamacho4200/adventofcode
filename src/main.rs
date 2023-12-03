31bbb5bf17fae0ec52829e589df7e3f5af689e46
Henry Camacho <hcamacho@linkedin.com>
2023-12-03 16:25:32 -0600

    various day 3 activities

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![feature(str_split_remainder)]
+#![feature(exclusive_range_pattern)]
 
 mod aoc202201;
 mod aoc202202;
 mod aoc202301;
 mod aoc202302_1;
 mod aoc202302_2;
+mod aoc202302_1_attempt2;
+mod aoc202303_1;
+mod aoc202303_2;
 
 fn main() {
     println!("Hello, world!");
